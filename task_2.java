e4884361b72e3d2811227f2d68f3b5759d7429a7
RenatDavletshin <immortal1307@gmail.com>
2023-02-20 01:35:40 +0500

    Task_2_end

--- task_2.java
+++ task_2.java
@@ -1,3 +1,19 @@
+import java.io.*;
+ 
 public class task_2 {
-    
+  
+    public static void main(String[] args) {
+          
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("file.txt")))
+        {
+            for (int i = 0; i < 100; i++) {
+            String text = "TEST\n";
+            bw.write(text);
+            }
+        }
+        catch(IOException ex){
+              
+            System.out.println(ex.getMessage());
+        } 
+    } 
 }
